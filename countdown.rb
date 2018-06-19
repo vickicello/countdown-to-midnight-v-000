ea76bab3576721692bc6602e232384c667a1f501
Vicki Hamm <vicki.cello@gmail.com>
2018-06-19 17:21:00 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
-#write your code here
+def countdown(n)
+  n = 10
+  until n == 0
+  puts "#{n} SECOND(S)!"
+  n -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(n)
+  sleep 5
 end
